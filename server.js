a4688033b9f3d829ba0185c1f695b6754d31033c
mn1701 <anhminh1701@gmail.com>
2024-04-28 00:05:15 -0700

    minh

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.get('/api/shifts', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
