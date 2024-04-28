044e66b68fd219f4e5dfd4940340a30cf6280efb
mn1701 <anhminh1701@gmail.com>
2024-04-27 22:20:15 -0700

    script change

--- public/scripts.js
+++ public/scripts.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetch('/api/shifts')
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(data => {
         const tableBody = document.getElementById('shiftsTable').getElementsByTagName('tbody')[0];
-        data.forEach(row => {
-            // Convert Excel serial date to JavaScript Date object
-            const date = convertExcelDate(row['__EMPTY_1']);
-            
-            // Check if the date is valid before appending the row
-            if (!isNaN(date.getTime())) { // This checks if the date is valid
-                let newRow = tableBody.insertRow();
-                let cell1 = newRow.insertCell(0);
-                let cell2 = newRow.insertCell(1);
-                let cell3 = newRow.insertCell(2);
-                cell1.textContent = row['__EMPTY'];
-                cell2.textContent = date.toLocaleDateString(); // Use the valid date
-                cell3.textContent = row['__EMPTY_2'];
+        data.slice(0).forEach(row => {
+            let newRow = tableBody.insertRow();
+            let cell1 = newRow.insertCell(0);
+            let cell2 = newRow.insertCell(1);
+            let cell3 = newRow.insertCell(2);
+            cell1.textContent = row['Shift Time']; // Shift time
+            if (row['Date']) {
+                const date = new Date((row['Date'] - (25567 + 2)) * 86400000);
+                date.setMinutes(date.getMinutes() + date.getTimezoneOffset()); // Adjust for timezone
+                cell2.textContent = date.toLocaleDateString();
+            } else {
+                cell2.textContent = '';
             }
+            cell3.textContent = row['Volunteers']; // Volunteers names
         });
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
 });
-
-// Helper function to convert Excel date serial numbers to JS Date objects
-function convertExcelDate(serial) {
-    return new Date(Date.UTC(0, 0, serial - 1, 0, 0, 0));
-}
